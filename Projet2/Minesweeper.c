08bcf97ddca9051042435bb099e09493da0a949a
unknown <trosselet@gamingcampus.local>
2024-03-12 12:32:25 +0100

    Changement

---
+++ Projet2/Minesweeper.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+const int board_length = 4;
+
+void Grid(){
+    int i;
+    int j;
+    for (i = 0; i <= board_length; i++){
+        char row[] = {}
+        for (j = 0; j<= board_length; j++){
+            row[j] = '.'
+        }
+    }
+}
+
+int main(){
+    return 0
+}
